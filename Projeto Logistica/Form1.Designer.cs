3f26865e48beb09ff816821847c84da3cd52e87f
vinicius.galhardo <vinicius.galhardo@TAU0588410W10-1>
2024-08-30 11:02:50 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Logistica/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Projeto_Logistica
+{
+    partial class PaginaInicial
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.btnPaginaInicial = new System.Windows.Forms.Button();
+            this.btnClientes = new System.Windows.Forms.Button();
+            this.btnTransportador = new System.Windows.Forms.Button();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnParceiros = new System.Windows.Forms.Button();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnlLogin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlLogin.Controls.Add(this.pictureBox2);
+            this.pnlLogin.Controls.Add(this.pictureBox1);
+            this.pnlLogin.Controls.Add(this.btnSobre);
+            this.pnlLogin.Controls.Add(this.btnParceiros);
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.btnCadastro);
+            this.pnlLogin.Controls.Add(this.btnTransportador);
+            this.pnlLogin.Controls.Add(this.btnClientes);
+            this.pnlLogin.Controls.Add(this.btnPaginaInicial);
+            this.pnlLogin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnlLogin.ForeColor = System.Drawing.SystemColors.Control;
+            this.pnlLogin.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(834, 460);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // btnPaginaInicial
+            // 
+            this.btnPaginaInicial.FlatAppearance.BorderSize = 0;
+            this.btnPaginaInicial.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaginaInicial.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPaginaInicial.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPaginaInicial.Location = new System.Drawing.Point(133, 0);
+            this.btnPaginaInicial.Name = "btnPaginaInicial";
+            this.btnPaginaInicial.Size = new System.Drawing.Size(84, 28);
+            this.btnPaginaInicial.TabIndex = 0;
+            this.btnPaginaInicial.Text = "HOME";
+            this.btnPaginaInicial.UseVisualStyleBackColor = true;
+            // 
+            // btnClientes
+            // 
+            this.btnClientes.FlatAppearance.BorderSize = 0;
+            this.btnClientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClientes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClientes.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClientes.Location = new System.Drawing.Point(202, 1);
+            this.btnClientes.Name = "btnClientes";
+            this.btnClientes.Size = new System.Drawing.Size(80, 28);
+            this.btnClientes.TabIndex = 1;
+            this.btnClientes.Text = "CLIENTES";
+            this.btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // btnTransportador
+            // 
+            this.btnTransportador.FlatAppearance.BorderSize = 0;
+            this.btnTransportador.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTransportador.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTransportador.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTransportador.Location = new System.Drawing.Point(276, 1);
+            this.btnTransportador.Name = "btnTransportador";
+            this.btnTransportador.Size = new System.Drawing.Size(138, 28);
+            this.btnTransportador.TabIndex = 2;
+            this.btnTransportador.Text = "TRANSPORTADOR";
+            this.btnTransportador.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnCadastro.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCadastro.FlatAppearance.BorderSize = 0;
+            this.btnCadastro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastro.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastro.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCadastro.Location = new System.Drawing.Point(737, 0);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(97, 37);
+            this.btnCadastro.TabIndex = 3;
+            this.btnCadastro.Text = "Cadastrar-se";
+            this.btnCadastro.UseVisualStyleBackColor = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLogin.Location = new System.Drawing.Point(644, 0);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(87, 37);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnParceiros
+            // 
+            this.btnParceiros.FlatAppearance.BorderSize = 0;
+            this.btnParceiros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnParceiros.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParceiros.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnParceiros.Location = new System.Drawing.Point(410, 1);
+            this.btnParceiros.Name = "btnParceiros";
+            this.btnParceiros.Size = new System.Drawing.Size(94, 28);
+            this.btnParceiros.TabIndex = 5;
+            this.btnParceiros.Text = "PARCEIROS";
+            this.btnParceiros.UseVisualStyleBackColor = true;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Location = new System.Drawing.Point(498, 3);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(75, 23);
+            this.btnSobre.TabIndex = 6;
+            this.btnSobre.Text = "SOBRE";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            this.btnSobre.Click += new System.EventHandler(this.btnSobre_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Projeto_Logistica.Properties.Resources.logo_4;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::Projeto_Logistica.Properties.Resources.logo_4_1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(156, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(666, 414);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // PaginaInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 461);
+            this.Controls.Add(this.pnlLogin);
+            this.Name = "PaginaInicial";
+            this.Text = "Pagina Inicial";
+            this.pnlLogin.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnTransportador;
+        private System.Windows.Forms.Button btnClientes;
+        private System.Windows.Forms.Button btnPaginaInicial;
+        private System.Windows.Forms.Button btnParceiros;
+        private System.Windows.Forms.Button btnSobre;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
